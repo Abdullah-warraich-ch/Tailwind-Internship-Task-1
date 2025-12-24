de9392d719f97ec8032590b2c73263e995c69fbb
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2025-12-24 12:09:27 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html"], // adjust according to your project files
+  theme: {
+    extend: {
+      fontFamily: {
+        work: ['"Work Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
